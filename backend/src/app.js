7adcd2d92f61677690dcabfd9e5c2f35d389ac95
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-11-06 13:11:00 +0100

    Conexión a la base de satos y cración de modelo de usuario

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,34 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
+
+
+const PORT = 5000;
+
+require('./database');
+
+
+//Middlewares
+
+const customMiddleware=(req, res, next)=>{
+    console.log("middleware");
+    next();
+}
+
+app.use(customMiddleware);
+
+
+//Routes
+app.get('/',(req, res)=>{
+    res.send('Hello world!');
+});
+
+app.get('/about',(req, res)=>{
+    res.send('about');
+});
+
+
+//Server
+app.listen(PORT,()=>{
+    console.log("Server in port ", PORT);
+}); 
