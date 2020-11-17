2728d64a773d0a73fb51cc175a89de719c0214b5
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-11-17 15:06:06 +0100

    Creada vista de la pagina de inicio (timeline)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Home = () => {
 
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Javier</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1571458837028-f73e8bce31df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" />
+
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>This is my amazing post</p>
+                    <input type="text" placeholder="Add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Javier</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1571458837028-f73e8bce31df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" />
+
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>This is my amazing post</p>
+                    <input type="text" placeholder="Add a comment" />
+                </div>
+            </div>
+
+        </div>
     );
 };
 
