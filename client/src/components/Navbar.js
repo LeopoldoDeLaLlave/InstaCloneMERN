9eba9530b24b66f87a76ea8f04b6cd53b0c596b6
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-11-24 13:15:02 +0100

    Creado logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React, {useContext} from 'react';
-import {Link } from 'react-router-dom';
+import {Link, useHistory } from 'react-router-dom';
 import {UserContext} from '../App';
 
 const NavBar = () => {
     const {state, dispatch} = useContext(UserContext);
-
+    const history = useHistory(); 
     const renderList = ()=>{
         //Si hay algún valor (user) en el state se muestran los links de perfil y crrar post
         if(state){
             return[
                 <li><Link to="/profile">Profile</Link></li>,
-                <li><Link to="/create">Create Post</Link></li>
+                <li><Link to="/create">Create Post</Link></li>,
+                <li>
+                    <button className="btn waves-effect waves-light #d50000 red accent-4" 
+                    onClick={()=>{
+                        localStorage.clear();
+                        dispatch({type:"CLEAR"});
+                        history.push("/signin");
+                    }}>
+                        Logout
+                    </button>
+                </li>
             ];
             //Si no hay ningún valor (user) en el state se muestran los links de login y registro
         }else{
